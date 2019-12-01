4f5256e66356d547bec88c9f4d17a10837bcfff9
Yan Oliveira <yan.brito@hotmail.com>
2019-12-01 01:46:22 -0300

    Database configuration

--- config/dev.exs
+++ config/dev.exs
@@ -3,7 +3,7 @@ use Mix.Config
 # Configure your database
 config :twitter, Twitter.Repo,
   username: "postgres",
-  password: "postgres",
+  password: "docker",
   database: "twitter_dev",
   hostname: "localhost",
   show_sensitive_data_on_connection_error: true,
